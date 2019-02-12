a00f3dc85e77ef732269b2d73757431ccd8796db
thiagoN13 <thiago.nogueira187@gmail.com>
2019-02-12 13:32:46 -0300

    [FIX] Fixed color loading

--- src/assets/js/orbit.js
+++ src/assets/js/orbit.js
@@ -34,7 +34,6 @@ function init() {
   generateCubes()
 
   window.addEventListener('resize', onWindowResize, false)
-  renderer.domElement.addEventListener('click', onDocumentMouseDown, true);
 }
 
 function onWindowResize() {
@@ -53,34 +52,58 @@ function render () {
   renderer.render(scene, camera)
 }
 
+function generateMesh (wall, column, index) {
+  // geometry
+  const geometry = new THREE.BoxGeometry(20, 20, 20, 0)
+  // material
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    vertexColors: THREE.FaceColors
+  })
+
+  // colors
+  const red = new THREE.Color(1, 0, 0)
+  const green = new THREE.Color(0, 1, 0)
+  const blue = new THREE.Color(0, 0, 1)
+  const yellow = new THREE.Color(1, 1, 0)
+  const orange = new THREE.Color(1, 0.5, 0)
+  const white = new THREE.Color(1, 1, 1)
+  const lightBlue = new THREE.Color(0, 1, 1)
+  const colors = [red, green, blue, yellow, orange, lightBlue, white]
+
+  for (let i = 0; i < colors.length - 1; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (!geometry.faces[2 * i + j]) continue
+
+      geometry.faces[2 * i + j].color = colors[i]
+    }
+  }
+
+  const cube = new THREE.Mesh(geometry, material)
+  cube.name = `${wall}.${column}.${index}`
+  cube.position.x = index * 21
+  cube.position.y = column * 21
+  cube.position.z = wall * 21
+  console.log(`PAREDE: ${wall}, COLUNA: ${column}, ITEM: ${index}`)
+
+  scene.add(cube)
+
+  cube.on('click', function (ev) {
+    // console.log(ev)
+    // console.log(`PAREDE: ${wall}, COLUNA: ${column}, ITEM: ${index}`)
+
+    console.log('%c' + cube.name + '%c => click', 'color: #fff; background: #41b882; padding: 3px 4px;', 'color: #41b882; background: #fff;');
+  })
+
+  document.body.appendChild( renderer.domElement )
+}
+
 function generateCubes () {
   for (let wall = 0; wall < 3; wall++) {
     for (let column = 0; column < 3; column++) {
       for (let index = 0; index < 3; index++) {
-        const geometry = new THREE.BoxBufferGeometry(20, 20, 20)
-        const cube = new THREE.Mesh( geometry )
-        cube.name = `${wall}.${column}.${index}`
-        cube.position.x = index * 21
-        cube.position.y = column * 21
-        cube.position.z = wall * 21
-        console.log(`PAREDE: ${wall}, COLUNA: ${column}, ITEM: ${index}`)
-        cube.callback = function() { console.log( this.name ); }
-
-        scene.add(cube)
-
-        cube.on('click', function (ev) {
-          console.log(ev)
-          // console.log(`PAREDE: ${wall}, COLUNA: ${column}, ITEM: ${index}`)
-
-          console.log('%c' + cube.name + '%c => click', 'color: #fff; background: #41b882; padding: 3px 4px;', 'color: #41b882; background: #fff;');
-        })
-
-        document.body.appendChild( renderer.domElement )
+        generateMesh(wall, column, index)
       }
     }
   }
-}
-
-function onDocumentMouseDown (event) {
-  // console.log(event, scene)
 }
